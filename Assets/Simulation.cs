67b36760a12a377f4b2096841e490537c42328f6
Alexandre Sajus <asajus47@gmail.com>
2023-03-04 16:51:24 +0100

    :tada: Adds Logging

--- Assets/Simulation.cs
+++ Assets/Simulation.cs
@@ -14,6 +14,15 @@ public class Simulation : MonoBehaviour
     public GameObject Rabbit;
     public GameObject Fox;
     public GameObject Grass;
+    private float time;
+    // Create a list to save the number of foxes each second
+    public List<int> foxes = new List<int>();
+    // Create a list to save the number of rabbits each second
+    public List<int> rabbits = new List<int>();
+    // Create a list to save the average speed of foxes each second
+    public List<float> fox_speed = new List<float>();
+    // Create a list to save the average speed of rabbits each second
+    public List<float> rabbit_speed = new List<float>();
     // Start is called before the first frame update
     void Start()
     {
@@ -62,6 +71,46 @@ public class Simulation : MonoBehaviour
         if (Random.Range(0f, 1f) < grass_spawn_rate)
         {
             Instantiate(Grass, new Vector3(Random.Range(-50f, 50f), 1f, Random.Range(-50f, 50f)), Quaternion.Euler(0, Random.Range(0f, 360f), 0));
+        }
+
+        time += Time.deltaTime;
+
+        // Each second
+        if (time > 1)
+        {
+            time = 0;
+            // Get the foxes
+            GameObject[] foxes_objects = GameObject.FindGameObjectsWithTag("Fox");
+            // Get the rabbits
+            GameObject[] rabbits_objects = GameObject.FindGameObjectsWithTag("Rabbit");
+
+            // Get the average speed of foxes
+            float fox_current_speed = 0;
+            foreach (GameObject fox in foxes_objects)
+            {
+                fox_current_speed += fox.GetComponent<Fox>().speed;
+            }
+            fox_current_speed /= foxes_objects.Length;
+
+            // Get the average speed of rabbits
+            float rabbit_current_speed = 0;
+            foreach (GameObject rabbit in rabbits_objects)
+            {
+                rabbit_current_speed += rabbit.GetComponent<Rabbit>().speed;
+            }
+            rabbit_current_speed /= rabbits_objects.Length;
+
+            // Add the number of foxes, rabbits, average speed of foxes and average speed of rabbits to the lists
+            this.foxes.Add(foxes_objects.Length);
+            this.rabbits.Add(rabbits_objects.Length);
+            this.fox_speed.Add(fox_current_speed);
+            this.rabbit_speed.Add(rabbit_current_speed);
+
+            // Save the foxes, rabbits, average speed of foxes and average speed of rabbits to a file
+            System.IO.File.WriteAllLines("foxes.txt", foxes.ConvertAll(i => i.ToString()).ToArray());
+            System.IO.File.WriteAllLines("rabbits.txt", rabbits.ConvertAll(i => i.ToString()).ToArray());
+            System.IO.File.WriteAllLines("fox_speed.txt", fox_speed.ConvertAll(i => i.ToString()).ToArray());
+            System.IO.File.WriteAllLines("rabbit_speed.txt", rabbit_speed.ConvertAll(i => i.ToString()).ToArray());
         }
     }
 }
